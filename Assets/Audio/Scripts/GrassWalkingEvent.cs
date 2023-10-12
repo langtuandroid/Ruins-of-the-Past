5817b9695d1ca493a02c43660b7fe6e0c8713bb7
Jelmerrr <Jelljel@hotmail.com>
2023-10-12 15:03:51 +0200

    Code review changes

--- Assets/Audio/Scripts/GrassWalkingEvent.cs
+++ Assets/Audio/Scripts/GrassWalkingEvent.cs
@@ -15,13 +15,6 @@ public class GrassWalkingEvent : MonoBehaviour
     public GameObject foxObject;
     public GameObject wolfObject;
 
-    //Private variables
-    private float foxZvol;
-    private float foxDist;
-
-    private float wolfZvol;
-    private float wolfDist;
-
     private void Start()
     {
         //Initialize events
@@ -31,80 +24,53 @@ public class GrassWalkingEvent : MonoBehaviour
 
     private void FixedUpdate()
     {
-        //Grab velocity of fox and wolf
-        Rigidbody foxRB = foxObject.GetComponent<Rigidbody>();
-        foxZvol = foxRB.velocity.z;
-
-        Rigidbody wolfRB = wolfObject.GetComponent<Rigidbody>();
-        wolfZvol = wolfRB.velocity.z;
-
-        CheckGround();
-        UpdateSound();
+        //Update sound events
+        UpdateSound(foxFootstepsGrass, VelocityZ(foxObject), GroundDistance(foxObject));
+        UpdateSound(wolfFootstepsGrass, VelocityZ(wolfObject), GroundDistance(wolfObject));
     }
 
-    private void CheckGround() //Checks if fox and wolf are above ground
+    private float VelocityZ(GameObject gameObject) //Return velocity of object
     {
-        //Create raycasthit
-        RaycastHit foxHit;
-        RaycastHit wolfHit;
-
-        //Fox raycast
-        Rigidbody foxRB = foxObject.GetComponent<Rigidbody>();
-        Vector3 foxOrigin = foxRB.position;
-        Ray foxDownRay = new Ray(foxOrigin, -Vector3.up);
-
-        //Wolf raycast
-        Rigidbody wolfRB = wolfObject.GetComponent<Rigidbody>();
-        Vector3 wolfOrigin = wolfRB.position;
-        Ray wolfDownRay = new Ray(wolfOrigin, -Vector3.up);
-
-        //Execute raycasts
-        if (Physics.Raycast(foxDownRay, out foxHit))
-        {
-            foxDist = foxHit.distance;
-        }
-        if (Physics.Raycast(wolfDownRay, out wolfHit))
-        {
-            wolfDist = wolfHit.distance;
-        }
+        var objectRB = gameObject.GetComponent<Rigidbody>();
+        return (objectRB.velocity.z);
     }
 
-    private void UpdateSound() //Updates sound events according to object state
+    private float GroundDistance(GameObject gameObject) //Return distance from ground for object
     {
-        //Check if proper conditions are met for fox
-        if(enable == true && foxZvol <= -0.5 && foxDist <= 0.1 || enable == true && foxZvol >= 0.5 && foxDist <= 0.1)
+        //Create raycast
+        RaycastHit rcHit;
+
+        //Setup vector
+        var objectRB = gameObject.GetComponent<Rigidbody>();
+        Vector3 originPos = objectRB.position;
+        Ray downRay = new Ray(originPos, -Vector3.up);
+
+        //Execute vector
+        if (Physics.Raycast(downRay, out rcHit))
+        {
+            return (rcHit.distance);
+        }
+        else return 0; //If raycast does not hit ground set failsafe
+    }
+
+    private void UpdateSound(EventInstance eventInstance, float zVelocity, float dist) //Update sound events according to object state
+    {
+        //Check if proper conditions are met
+        if ((enable == true && zVelocity <= -0.5 && dist <= 0.1) || (enable == true && zVelocity >= 0.5 && dist <= 0.1))
         {
             //Check current playback state
-            PLAYBACK_STATE foxPlaybackState;
-            foxFootstepsGrass.getPlaybackState(out foxPlaybackState);
-            if (foxPlaybackState == PLAYBACK_STATE.STOPPED)
+            PLAYBACK_STATE currentSoundState;
+            eventInstance.getPlaybackState(out currentSoundState);
+            if (currentSoundState == PLAYBACK_STATE.STOPPED)
             {
                 //Start event
-                foxFootstepsGrass.start();
+                eventInstance.start();
             }
         }
         else
         {
             //Stop event
-            foxFootstepsGrass.stop(STOP_MODE.ALLOWFADEOUT);
-        }
-
-        //Check if proper conditions are met for wolf
-        if (enable == true && wolfZvol <= -0.5 && wolfDist <= 0.1 || enable == true && wolfZvol >= 0.5 && wolfDist <= 0.1)
-        {
-            //Check current playback state
-            PLAYBACK_STATE wolfPlaybackState;
-            wolfFootstepsGrass.getPlaybackState(out wolfPlaybackState);
-            if (wolfPlaybackState == PLAYBACK_STATE.STOPPED)
-            {
-                //Start event
-                wolfFootstepsGrass.start();
-            }
-        }
-        else
-        {
-            //Stop event
-            wolfFootstepsGrass.stop(STOP_MODE.ALLOWFADEOUT);
+            eventInstance.stop(STOP_MODE.ALLOWFADEOUT);
         }
     }
 }

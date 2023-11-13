a935883be02d180ad072e7db5e309e19817c3aa6
amy <amywinterfox@gmail.com>
2023-11-13 15:45:06 +0100

    work on fixing malbers layers

--- Assets/Scripts/Features/Time Rift/TimePiece.cs
+++ Assets/Scripts/Features/Time Rift/TimePiece.cs
@@ -1,72 +1,83 @@
 using System.Collections.Generic;
-using System.Linq;
-using UnityEditor;
+using MalbersAnimations;
+using MalbersAnimations.Controller;
+using MalbersAnimations.Scriptables;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class TimePiece : MonoBehaviour
+namespace Features.Time_Rift
 {
-    [SerializeField] private GameObject[] players;
-
-    [SerializeField] private Camera activeCamera;
-
-    [SerializeField] private Camera hiddenCamera;
-
-    [SerializeField] private int hiddenSceneIndex;
-
-    private bool isInPast = false;
-
-    private static readonly int TimepieceTexture = Shader.PropertyToID("_TimepieceTexture");
-
-    private List<Rigidbody> _capsuleColliders = new();
-
-    private void Start()
+    public class TimePiece : MonoBehaviour
     {
-        foreach (var player in players)
+        [SerializeField] private GameObject[] players;
+
+        [SerializeField] private Camera activeCamera;
+
+        [SerializeField] private Camera hiddenCamera;
+
+        [SerializeField] private int hiddenSceneIndex;
+
+        [SerializeField] private LayerMask activePhysicsLayerMask;
+
+        [SerializeField] private LayerMask hiddenPhysicsLayerMask;
+
+        private bool _isInPast;
+
+        private static readonly int TimepieceTexture = Shader.PropertyToID("_TimepieceTexture");
+
+        private List<Rigidbody> _capsuleColliders = new();
+
+        private void Start()
         {
-            _capsuleColliders.Add(player.GetComponent<Rigidbody>());
+            foreach (var player in players)
+            {
+                _capsuleColliders.Add(player.GetComponent<Rigidbody>());
+            }
+
+            SceneManager.LoadScene(hiddenSceneIndex, LoadSceneMode.Additive);
+
+            var texture = new RenderTexture(Screen.width, Screen.height, 24);
+            Shader.SetGlobalTexture(TimepieceTexture, texture);
+            hiddenCamera.targetTexture = texture;
         }
 
-        SceneManager.LoadScene(hiddenSceneIndex, LoadSceneMode.Additive);
-
-        var texture = new RenderTexture(Screen.width, Screen.height, 24);
-        Shader.SetGlobalTexture(TimepieceTexture, texture);
-        hiddenCamera.targetTexture = texture;
-    }
-
-    private void Update()
-    {
-        //hiddenCamera.Render();
-
-        if (!Input.GetKeyDown(KeyCode.R)) return; // TODO: Convert to new input system
-
-        activeCamera.targetTexture = hiddenCamera.targetTexture;
-        hiddenCamera.targetTexture = null;
-
-        // Swap cameras
-        (activeCamera, hiddenCamera) = (hiddenCamera, activeCamera);
-
-        var mask = LayerMask.NameToLayer(isInPast ? "Present" : "Past");
-        foreach (var p in players)
+        private void Update()
         {
-            SetLayerRecursively(p, mask);
-        }
+            if (!Input.GetKeyDown(KeyCode.R)) return; // TODO: Convert to new input system
 
-        foreach (var c in _capsuleColliders)
-        {
-            c.excludeLayers = mask;
-        }
+            _isInPast = !_isInPast;
 
-        isInPast = !isInPast;
-    }
+            activeCamera.targetTexture = hiddenCamera.targetTexture;
+            hiddenCamera.targetTexture = null;
 
-    private static void SetLayerRecursively(GameObject o, int layer)
-    {
-        o.layer = layer;
-        for (var i = 0; i < o.transform.childCount; i++)
-        {
-            var child = o.transform.GetChild(i);
-            SetLayerRecursively(child.gameObject, layer);
+            // Swap cameras
+            (activeCamera, hiddenCamera) = (hiddenCamera, activeCamera);
+
+            var oldMask = !_isInPast ? activePhysicsLayerMask : hiddenPhysicsLayerMask;
+            var newMask = _isInPast ? activePhysicsLayerMask : hiddenPhysicsLayerMask;
+            foreach (var c in _capsuleColliders)
+            {
+                c.excludeLayers = newMask;
+            }
+
+            foreach (var player in players)
+            {
+                player.GetComponent<MAnimal>().groundLayer = new LayerReference(LayerMask.NameToLayer("Past"));
+
+                var colliders = player.GetComponentsInChildren<Collider>();
+                foreach (var capsuleCollider in colliders)
+                {
+                    capsuleCollider.includeLayers = oldMask;
+                    capsuleCollider.excludeLayers = newMask;
+                }
+
+                var rigidbodies = player.GetComponentsInChildren<Rigidbody>();
+                foreach (var rigidBody in rigidbodies)
+                {
+                    rigidBody.includeLayers = oldMask;
+                    rigidBody.excludeLayers = newMask;
+                }
+            }
         }
     }
 }
